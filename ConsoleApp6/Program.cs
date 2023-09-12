c56025d6e523dc66ca406431baa0dc4655e9cd03
Leonard Lume <leonard.lume@tktk.ee>
2023-09-12 18:40:25 +0300

    create new app

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your name:"); //Output
+//string - sone
+string userName = Console.ReadLine(); //input
+
+Console.WriteLine("Hello" + "," + userName + "!"); //Output
+
+//stribg interpolation
+Console.WriteLine($"Hello, {userName} !"); //Output
+
+
